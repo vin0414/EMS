cfacc1b237a98b6502dcb673a12649377aecbf33
vin0414 <vinmogate@gmail.com>
2024-05-08 11:41:19 +0800

    updates

--- app/Views/layouts/custom_pagination.php
+++ app/Views/layouts/custom_pagination.php
@@ -1,38 +1,35 @@
 <?php $pager->setSurroundCount(2) ?>
-<style>
-    .list{margin-left:5px;}
-</style>
 <nav aria-label="Page navigation">
     <ul class="pagination">
     <?php if ($pager->hasPrevious()) : ?>
-        <li class="list">
-            <a href="<?= $pager->getFirst() ?>" aria-label="<?= lang('Pager.first') ?>">
+        <li>
+            <a class="btn btn-outline-secondary btn-sm" href="<?= $pager->getFirst() ?>" aria-label="<?= lang('Pager.first') ?>">
                 <span aria-hidden="true"><?= lang('Pager.first') ?></span>
             </a>
         </li>
-        <li class="list">
-            <a href="<?= $pager->getPrevious() ?>" aria-label="<?= lang('Pager.previous') ?>">
+        <li>
+            <a class="btn btn-outline-secondary btn-sm" href="<?= $pager->getPrevious() ?>" aria-label="<?= lang('Pager.previous') ?>">
                 <span aria-hidden="true"><?= lang('Pager.previous') ?></span>
             </a>
         </li>
     <?php endif ?>
 
     <?php foreach ($pager->links() as $link): ?>
-        <li class="list" <?= $link['active'] ? 'class="active"' : '' ?>>
-            <a href="<?= $link['uri'] ?>">
+        <li <?= $link['active'] ? 'class="active"' : '' ?>>
+            <a  class="btn btn-outline-secondary btn-sm" href="<?= $link['uri'] ?>">
                 <?= $link['title'] ?>
             </a>
         </li>
     <?php endforeach ?>
 
     <?php if ($pager->hasNext()) : ?>
-        <li class="list">
-            <a href="<?= $pager->getNext() ?>" aria-label="<?= lang('Pager.next') ?>">
+        <li>
+            <a  class="btn btn-outline-secondary btn-sm" href="<?= $pager->getNext() ?>" aria-label="<?= lang('Pager.next') ?>">
                 <span aria-hidden="true"><?= lang('Pager.next') ?></span>
             </a>
         </li>
-        <li class="list">
-            <a href="<?= $pager->getLast() ?>" aria-label="<?= lang('Pager.last') ?>">
+        <li>
+            <a  class="btn btn-outline-secondary btn-sm" href="<?= $pager->getLast() ?>" aria-label="<?= lang('Pager.last') ?>">
                 <span aria-hidden="true"><?= lang('Pager.last') ?></span>
             </a>
         </li>
